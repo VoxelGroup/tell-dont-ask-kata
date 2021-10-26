1071d8ba69f113369961c90392e18d7c42ced05a
Luis Benedicto <lbenedicto@voxelgroup.net>
2021-10-26 16:14:15 +0200

    refact: extract method to Order type

--- Exeal.Katas.TellDontAsk/UseCase/OrderApprovalUseCase.cs
+++ Exeal.Katas.TellDontAsk/UseCase/OrderApprovalUseCase.cs
@@ -1,6 +1,4 @@
-﻿using Exeal.Katas.TellDontAsk.Domain;
-using Exeal.Katas.TellDontAsk.Exception;
-using Exeal.Katas.TellDontAsk.Repository;
+﻿using Exeal.Katas.TellDontAsk.Repository;
 
 namespace Exeal.Katas.TellDontAsk.UseCase
 {
@@ -15,24 +13,10 @@ namespace Exeal.Katas.TellDontAsk.UseCase
 
         public void Run(OrderApprovalRequest request)
         {
-            Order order = orderRepository.GetById(request.OrderId);
+            var order = orderRepository.GetById(request.OrderId);
 
-            if (order.Status.Equals(OrderStatus.Shipped))
-            {
-                throw new ShippedOrdersCannotBeChangedException();
-            }
+            order.Approve(request);
 
-            if (request.Approved && order.Status.Equals(OrderStatus.Rejected))
-            {
-                throw new RejectedOrderCannotBeApprovedException();
-            }
-
-            if (!request.Approved && order.Status.Equals(OrderStatus.Approved))
-            {
-                throw new ApprovedOrderCannotBeRejectedException();
-            }
-
-            order.Status = request.Approved ? OrderStatus.Approved : OrderStatus.Rejected;
             orderRepository.Save(order);
         }
     }
